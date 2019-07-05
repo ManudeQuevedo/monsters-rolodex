fd65e1205502a1f63f58eda6f0208a6e3c39b9d1
Manuel Matus <hola@manudequevedo.com>
2019-07-05 11:59:54 -0500

    Updates

---
+++ precache-manifest.5b7a7da30925b909664284ce7e776f2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "575c4717b34a54c4c4297db3f8b3f2ed",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ded02fa7ac0ce2afc040",
+    "url": "/monsters-rolodex/static/css/main.49fe2125.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ded02fa7ac0ce2afc040",
+    "url": "/monsters-rolodex/static/js/main.57cbdc70.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
